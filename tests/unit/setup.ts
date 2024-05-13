073d5b0d950d6eacd3528724f0fe839e1710e199
Jeffrey <JeffreyCorderoNY@gmail.com>
2024-05-13 16:28:03 -0400

    Fix retry for unit tests

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { execSync } = require("child_process");
 
 try {
-  console.log("Setting up tests.")
+   console.log("Setting up tests.");
    // Database will be on localhost:5433
    process.env.DATABASE_URL = process.env.DATABASE_TEST_URL;
 
@@ -10,24 +10,26 @@ try {
    execSync("docker build -t vitality_postgres_test_image .", { stdio: "ignore" },);
    execSync("docker run -d --name vitality_postgres_test -p 5433:5432 vitality_postgres_test_image", { stdio: "ignore" });
 
-    // Run all unit tests with at most 3 retries for potential time-based database conflicts
-    let retries = 1;
-    
-    while (retries <= 3) {
+   // Run all unit tests with at most 3 retries for potential time-based database conflicts
+   let retries = 1;
+
+   while (retries <= 3) {
       try {
-        execSync("npx jest --collect-coverage", { stdio: "inherit" });
-        break;
-    } catch (error) {
-        console.error(`Error running tests #${retries}:`, error);
-    }
-  }
+         execSync("npx jest --collect-coverage", { stdio: "inherit" });
+         break;
+      } catch (error) {
+         console.error(`Error running tests #${retries}:`, error);
+      }
+
+      retries++;
+   }
 } catch (error) {
    console.error("Error setting up tests:", error);
    process.exit(1);
 } finally {
    // Cleanup the database container
    try {
-    console.log("Cleaning up tests.")
+      console.log("Cleaning up tests.");
       execSync("docker stop vitality_postgres_test && docker rm vitality_postgres_test", { stdio: "ignore" });
    } catch (error) {
       console.error("Error cleaning up tests:", error);
